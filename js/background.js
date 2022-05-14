552da8757db20acc03f845037e49fdb58f5ca344
home <chosd1123@naver.com>
2022-05-15 00:41:22 +0900

    #todolist

--- js/background.js
+++ js/background.js
@@ -4,10 +4,10 @@ const images = [
     "2.jpeg",
 ];
 
-const chosenImages = images[Math.floor(Math.random() * images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImages}`;
+bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
---
+++ js/todo.js
@@ -0,0 +1,30 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function deletTodo(event){
+    event.target.parentElement.innerText
+}
+
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deletTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo= toDoInput.value;
+    toDoList.value = "";
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
